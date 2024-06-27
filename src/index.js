0bf5500f471e4f2d90293efabb20e502d43f36ac
BlackCat64 <150248561+BlackCat64@users.noreply.github.com>
2024-06-27 11:20:33 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const {Client, IntentsBitField} = require("discord.js");
+
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent,
+    ]
+})
+
+client.on("ready", (c) => {
+    console.log(`Logged in as ${c.user.tag}!`);
+});
+
+client.on("messageCreate", (msg) => {
+    if (msg.author.bot)
+        return;
+
+    if (msg.content === "ping") {
+        msg.reply("pong");
+    }
+});
+
+client.login(process.env.TOKEN);
