e20c7496ea15515cda5198af89c74371ec19e947
BlackCat64 <150248561+BlackCat64@users.noreply.github.com>
2024-06-29 16:19:33 +0100

    Removed 'add' command and old command handling code

---
+++ src/commands/hello.js
@@ -0,0 +1,11 @@
+const {SlashCommandBuilder} = require("discord.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("hello")
+        .setDescription("Bot gives a friendly reply!"),
+    run: ({interaction}) => {
+        interaction.reply(`Hello there, user ${interaction.user}`);
+        console.log("hello command successful!")
+    }
+}
--- src/index.js
+++ src/index.js
@@ -22,31 +22,6 @@ client.on("ready", (c) => {
     console.log(`Logged in as ${c.user.tag}!`);
 });
 
-client.on("interactionCreate", (interaction) => {
-    if (!interaction.isChatInputCommand()) return;
-
-    let cmd = interaction.commandName;
-    switch (cmd) {
-        case "hello":
-            interaction.reply(`Hello there, user ${interaction.user}`)
-            break;
-        case "add":
-            const num1 = interaction.options.get("num1").value;
-            const num2  = interaction.options.get("num2").value;
-            const num3 = interaction.options.get("num3")?.value;
-            let sum = num1 + num2;
-            if (num3 === undefined) // If no 3rd argument was supplied
-                interaction.reply(`${num1} + ${num2} = ${sum}`);
-            else {
-                sum += num3; // If a 3rd argument was supplied
-                interaction.reply(`${num1} + ${num2} + ${num3} = ${sum}`);
-            }
-            break;
-        default:
-            console.log("Command not found.")
-    }
-})
-
 client.on("messageCreate", (msg) => {
     if (msg.author.bot)
         return;
